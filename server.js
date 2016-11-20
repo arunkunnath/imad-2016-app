2c172944775f5faad26f1d8f248ed1552eebe7c9
arunkunnath <arunkunnath007@gmail.com>
2016-11-20 18:28:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('Article_one.html', function (req, res) {
+app.get('/Article_one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article_one.html'));
 });
 
